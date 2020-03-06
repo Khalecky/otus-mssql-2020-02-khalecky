29ed29d1db258e0e891f9657defa030016fa7e17
romario <halecky@mail.ru>
2020-03-06 23:45:44 +0700

    homework2: refactor scripts

--- hw02_select/SQLQuerys.sql
+++ hw02_select/SQLQuerys.sql
@@ -7,36 +7,34 @@ LEFT JOIN Purchasing.PurchaseOrders o ON o.SupplierID=s.SupplierID
 WHERE o.SupplierID IS NULL;
 
 --ѕродажи с названием мес€ца
-SELECT o.OrderID, DATENAME(m, o.OrderDate) OrderMonth,
-	((MONTH(o.OrderDate) - 1)/3 + 1) OrderQuart ,
+SELECT DISTINCT o.OrderID, o.OrderDate, DATENAME(m, o.OrderDate) OrderMonth,
+	(DatePart(q, o.OrderDate)) OrderQuart,
 	((MONTH(o.OrderDate) -1)/4 + 1) OrderThird
 FROM Sales.Orders o
 INNER JOIN Sales.OrderLines l ON l.OrderID = o.OrderID
-WHERE (l.UnitPrice > 100 OR l.Quantity > 20) AND NOT o.PickingCompletedWhen IS NULL
-GROUP BY o.OrderID, o.OrderDate
-ORDER BY OrderQuart, OrderThird, o.OrderDate
+WHERE (l.UnitPrice > 100 OR l.Quantity > 20) AND o.PickingCompletedWhen IS NOT NULL
+ORDER BY OrderQuart, OrderThird, o.OrderDate;
 
 --ѕродажи постраничные
-SELECT o.OrderID, DATENAME(m, o.OrderDate) OrderMonth,
-	((MONTH(o.OrderDate) - 1)/3 + 1) OrderQuart ,
+SELECT DISTINCT o.OrderID, o.OrderDate, DATENAME(m, o.OrderDate) OrderMonth,
+	(DatePart(q, o.OrderDate)) OrderQuart,
 	((MONTH(o.OrderDate) -1)/4 + 1) OrderThird
 FROM Sales.Orders o
 INNER JOIN Sales.OrderLines l ON l.OrderID = o.OrderID
-WHERE (l.UnitPrice > 100 OR l.Quantity > 20) AND NOT o.PickingCompletedWhen IS NULL
-GROUP BY o.OrderID, o.OrderDate
+WHERE (l.UnitPrice > 100 OR l.Quantity > 20) AND o.PickingCompletedWhen IS NOT NULL
 ORDER BY OrderQuart, OrderThird, o.OrderDate
-OFFSET 1000 ROWS FETCH NEXT 100 ROWS ONLY
+OFFSET 1000 ROWS FETCH NEXT 100 ROWS ONLY;
 
 -- «аказы поставщикам, которые были исполнены за 2014й год с доставкой Road Freight или Post
-select po.*, s.SupplierName, p.FullName
+select po.*, s.SupplierName, p.FullName ContactName
 from Purchasing.PurchaseOrders po
 left join Purchasing.Suppliers s on s.SupplierID=po.SupplierID
 left join Application.DeliveryMethods m on m.DeliveryMethodID=po.DeliveryMethodID
 left join Application.People p on p.PersonID=po.ContactPersonID 
 where 
 	IsOrderFinalized=1
-	and (m.DeliveryMethodName like 'Road Freight' or m.DeliveryMethodName like 'Post')
-	and YEAR(po.OrderDate)=2014
+	and m.DeliveryMethodName in ('Road Freight', 'Post')
+	and po.OrderDate >= '2014-01-01' and po.OrderDate < '2015-01-01'
 
 --10 последних по дате продаж с именем клиента и именем сотрудника, который оформил заказ
 select top 10 o.*, c.CustomerName, p.FullName StaffName
@@ -51,7 +49,7 @@ from Sales.Customers c
 inner join Sales.Orders o on o.CustomerID=c.CustomerID
 left join Sales.OrderLines ol on ol.OrderID=o.OrderID
 left join Warehouse.StockItems items on items.StockItemID=ol.StockItemID
-where items.StockItemName like 'Chocolate frogs 250g'
+where items.StockItemName = 'Chocolate frogs 250g'
 order by c.CustomerID
 
 
